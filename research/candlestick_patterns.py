f01987ff18b777dece9374388e7515f603a2acfd
Henry Lei <henrylei2000@gmail.com>
2023-03-01 16:43:43 -0800

    added momentum, volume, volatility, and trend indicators

--- research/candlestick_patterns.py
+++ research/candlestick_patterns.py
@@ -4,6 +4,7 @@ import mplfinance as mpf
 import talib # Import TA-Lib
 import matplotlib.pyplot as plt
 import numpy as np
+import ta  # https://github.com/bukosabino/ta/
 
 def visualize():
     # Set the ticker symbol and time interval
@@ -93,44 +94,115 @@ def candlesticks():
     fig.show()
 
 
+def get_tmt_tickers():
+    file = "tmt_nasdaq100.csv"
+    df = pd.read_csv(file)
+    return df['Ticker']
+
+
+def backtesting():
+    import numpy as np
+    tickers = get_tmt_tickers()
+    df = pd.DataFrame(columns=['Ticker', 'Profit', 'Base', 'Good'])
+    for t in tickers:
+        bt = ta_bbands(t)
+        if bt[1] > bt[2]:
+            bt.append(1)
+        else:
+            bt.append('')
+        df.loc[len(df)] = bt
+    print(df)
+    print(len(df), '-----------', df['Good'].value_counts()[1])
+
+    print(df['Profit'].sum())
+
 
 def ta_bbands(ticker, plot=False):
-    import pandas as pd
-    import ta
-
     # Load data
     df = yf.download(ticker, period="5d", interval='5m', progress=False)
 
-    # Calculate EMA
-    df['EMA5'] = ta.trend.ema_indicator(df['Close'], window=5)
-    df['EMA10'] = ta.trend.ema_indicator(df['Close'], window=10)
+    open, high, low, close, volume = df['Open'], df['High'], df['Low'], df['Close'], df['Volume']
+    
+    # ta features
+    # momentum
+    df['RSI'] = ta.momentum.rsi(close, window=14, fillna=False)
+    df['TSI'] = ta.momentum.tsi(close, window_slow=25, window_fast=13, fillna=False)
+    df['UO'] = ta.momentum.ultimate_oscillator(high, low, close, window1=7, window2=14, window3=28,
+                                               weight1=4.0, weight2=2.0, weight3=1.0, fillna=False)
+    df['STOCH'] = ta.momentum.stoch(high, low, close, window=14, smooth_window=3, fillna=False)
+    df['STOCH_SIGNAL'] = ta.momentum.stoch_signal(high, low, close, window=14, smooth_window=3)
+    df['STOCH_RSI'] = ta.momentum.stochrsi(close, window=14, smooth1=3, smooth2=3, fillna=False)
+    df['KAMA'] = ta.momentum.kama(close, window=10, pow1=2, pow2=30, fillna=False)
+    df['ROC'] = ta.momentum.roc(close, window=12, fillna=False)
+    df['AO'] = ta.momentum.awesome_oscillator(high, low, window1=5, window2=34, fillna=False)
+    df['WILLIAMS_R'] = ta.momentum.williams_r(high, low, close, lbp=14, fillna=False)
+    df['PPO'] = ta.momentum.ppo(close, window_slow=26, window_fast=12, window_sign=9, fillna=False)
+    df['PVO'] = ta.momentum.pvo(volume, window_slow=26, window_fast=12, window_sign=9, fillna=False)
 
-    # Calculate RSI
-    df['RSI'] = ta.momentum.rsi(df['Close'], window=14)
-    df['vwap'] = ta.volume.volume_weighted_average_price(df['High'], df['Low'], df['Close'], df['Volume'], window=14)
+    # volume
+    df['ADI'] = ta.volume.acc_dist_index(high, low, close, volume, fillna=False)
+    df['OBV'] = ta.volume.on_balance_volume(close, volume, fillna=False)
+    df['CMF'] = ta.volume.chaikin_money_flow(high, low, close, volume, window=20, fillna=False)
+    df['FI'] = ta.volume.force_index(close, volume, window=13, fillna=False)
+    df['EoM'] = ta.volume.ease_of_movement(high, low, volume, window=14, fillna=False)
+    df['SMA_EoM'] = ta.volume.sma_ease_of_movement(high, low, volume, window=14, fillna=False)
+    df['VPT'] = ta.volume.volume_price_trend(close, volume, fillna=False)
+    df['NVI'] = ta.volume.negative_volume_index(close, volume, fillna=False)
+    df['MFI'] = ta.volume.money_flow_index(high, low, close, volume, window=14, fillna=False)
+    df['VWAP'] = ta.volume.volume_weighted_average_price(high, low, close, volume, window=14)
 
-    # Calculate Bollinger Bands
-    df['BBL'] = ta.volatility.bollinger_lband(df['Close'], window=20, window_dev=2)
-    df['BBM'] = ta.volatility.bollinger_mavg(df['Close'], window=20)
-    df['BBU'] = ta.volatility.bollinger_hband(df['Close'], window=20, window_dev=2)
+    # volatility
+    df['ATR'] = ta.volatility.average_true_range(high, low, close, window=14, fillna=False)
+    df['BBM'] = ta.volatility.bollinger_mavg(close, window=20, fillna=False)
+    df['BBU'] = ta.volatility.bollinger_hband(close, window=20, window_dev=2, fillna=False)
+    df['BBL'] = ta.volatility.bollinger_lband(close, window=20, window_dev=2, fillna=False)
+    df['BBW'] = ta.volatility.bollinger_wband(close, window=20, window_dev=2, fillna=False)
+    df['BBP'] = ta.volatility.bollinger_pband(close, window=20, window_dev=2, fillna=False)
+    # Keltner ... skipped
+    # Donchian
+    df['DCM'] = ta.volatility.donchian_channel_mband(high, low, close, window=10, offset=0, fillna=False)
+    df['DCU'] = ta.volatility.donchian_channel_hband(high, low, close, window=20, offset=0, fillna=False)
+    df['DCL'] = ta.volatility.donchian_channel_lband(high, low, close, window=20, offset=0, fillna=False)
+    df['DCW'] = ta.volatility.donchian_channel_wband(high, low, close, window=10, offset=0, fillna=False)
+    df['DCP'] = ta.volatility.donchian_channel_pband(high, low, close, window=10, offset=0, fillna=False)
+
+    # trend
+    df['EMA5'] = ta.trend.ema_indicator(close, window=5)
+    df['EMA10'] = ta.trend.ema_indicator(close, window=10)
+    df['WMA10'] = ta.trend.wma_indicator(close, window=10, fillna=False)
+    df['MACD'] = ta.trend.macd(close, window_slow=26, window_fast=12, fillna=False)
+    df['MACD_SIGNAL'] = ta.trend.macd_signal(close, window_slow=26, window_fast=12, window_sign=9, fillna=False)
+    df['MACD_DIFF'] = ta.trend.macd_diff(close, window_slow=26, window_fast=12, window_sign=9, fillna=False)
+    # df['ADX'] = ta.trend.adx(high, low, close, window=14, fillna=False)
+    df['CCI'] = ta.trend.cci(high, low, close, window=20, constant=0.015, fillna=False)
+    df['DPO'] = ta.trend.dpo(close, window=20, fillna=False)
+    df['STC'] = ta.trend.stc(close, window_slow=50, window_fast=23, cycle=10, smooth1=3, smooth2=3, fillna=False)
+    df['PSAR_UP'] = ta.trend.psar_up(high, low, close, step=0.02, max_step=0.20, fillna=False)
+    df['PSAR_DOWN'] = ta.trend.psar_down(high, low, close, step=0.02, max_step=0.20, fillna=False)
+
+    # others
 
     # Define trading signals
-    ema_buy = (df['Close'] > df['EMA5']) & (df['EMA5'] > df['EMA10'])
-    ema_sell = (df['Close'] < df['EMA5']) & (df['EMA5'] < df['EMA10'])
-    rsi_buy = (df['RSI'] < 30) | (df['RSI'].shift(2) - df['RSI'] > 10)
-    rsi_sell = df['RSI'] > 72
-    bbands_buy = df['Close'] < df['BBL']
-    bbands_sell = df['Close'] > df['BBU']
-    vwap_buy = (df['Close'] > df['vwap']) & (df['Close'].shift(1) <= df['vwap'].shift(1))
-    vwap_sell = (df['Close'] < df['vwap']) & (df['Close'].shift(1) >= df['vwap'].shift(1))
+    ema_buy = (close > df['EMA5']) & (df['EMA5'] > df['EMA10'])
+    ema_sell = (close < df['EMA5']) & (df['EMA5'] < df['EMA10'])
+    rsi_buy = ((df['RSI'] < 30) | (df['PPO'] < -2))
+    rsi_sell = df['RSI'] > 70
+    bbands_buy = close < df['BBL']
+    bbands_sell = close > df['BBU']
+    vwap_buy = (close > df['VWAP']) & (close.shift(1) <= df['VWAP'].shift(1))
+    vwap_sell = (close < df['VWAP']) & (close.shift(1) >= df['VWAP'].shift(1))
+
     df['long_signal'] = bbands_buy & rsi_buy
     df['short_signal'] = bbands_sell & rsi_sell
 
     # Execute trades
     position = 0
     positions = []
+    entry = 1000
+    max_stakes = min(int(entry / df['Close'].mean()), 5)
+    max_stakes = max(2, max_stakes)
     for i in range(len(df)):
-        if df['long_signal'][i] and position < 5:
+        if df['long_signal'][i] and position < max_stakes:
             position += 1
             positions.append(1)
             print(f'buy at {df["Close"][i]}')
@@ -179,84 +251,21 @@ def ta_bbands(ticker, plot=False):
                     signal.append(np.nan)
             return signal
 
-        buy_signals = buy(df)
-        sell_signals = sell(df)
+        buy_points = buy(df)
+        sell_points = sell(df)
 
         bbs = df[['BBL', 'BBU']]  # DataFrame with two columns
 
         apds = [mpf.make_addplot(bbs.iloc[:-1, :], color='red'), mpf.make_addplot(df['RSI'][:-1])]
-        if np.isfinite(buy_signals).any():
-            apds.append(mpf.make_addplot(buy_signals[:-1], type='scatter', markersize=50, marker='^'))
-        if np.isfinite(sell_signals).any():
-            apds.append(mpf.make_addplot(sell_signals[:-1], type='scatter', markersize=50, marker='v'))
-        mpf.plot(df.iloc[:-1, :], type='candle', volume=True, addplot=apds)
+        if np.isfinite(buy_points).any():
+            apds.append(mpf.make_addplot(buy_points[:-1], type='scatter', markersize=50, marker='^'))
+        if np.isfinite(sell_points).any():
+            apds.append(mpf.make_addplot(sell_points[:-1], type='scatter', markersize=50, marker='v'))
+        mpf.plot(df.iloc[:-1, :], type='candle', volume=True, addplot=apds, show_nontrading=False)
         # mpf.plot(df.tail(100), type='candle', volume=True, mav=(10, 20), show_nontrading=False)
 
     return [ticker, profit, base]
 
-def get_tmt_tickers():
-    file = "tmt_nasdaq100.csv"
-    df = pd.read_csv(file)
-    return df['Ticker']
-
-def backtesting():
-    import numpy as np
-    tickers = get_tmt_tickers()
-    df = pd.DataFrame(columns=['Ticker', 'Profit', 'Base', 'Good'])
-    for t in tickers:
-        bt = ta_bbands(t)
-        if bt[1] > bt[2]:
-            bt.append(1)
-        else:
-            bt.append('')
-        df.loc[len(df)] = bt
-    print(df)
-    print(len(df), '-----------', df['Good'].value_counts()[1])
-
-    print(df['Profit'].sum())
-
-
-def animate_stock():
-    import matplotlib.pyplot as plt
-    from matplotlib.animation import FuncAnimation
-    import yfinance as yf
-
-    # Download stock data
-    stock = yf.download('AMZN', period='1mo')
-
-    # Remove rows with NaN or infinite values
-    stock = stock.dropna()
-
-    # Define figure and axis
-    fig, ax = plt.subplots()
-
-    # Initialize line plot
-    line, = ax.plot(stock.index, stock['Close'])
-
-    # Define animation function
-    def animate(i):
-        # Update data
-        line.set_data(stock.index[:i], stock['Close'][:i])
-        # Update x-axis limits
-        ax.set_xlim(stock.index[0], stock.index[-1])
-        # Update y-axis limits
-        ax.set_ylim(min(stock['Close']), max(stock['Close']))
-        # Set title
-        ax.set_title('AMZN Stock Price')
-        # Set x-label
-        ax.set_xlabel('Date')
-        # Set y-label
-        ax.set_ylabel('Price')
-        # Rotate x-axis labels
-        plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
-
-    # Animate plot
-    ani = FuncAnimation(fig, animate, frames=len(stock.index), interval=150)
-
-    # Show plot
-    plt.show()
-
-
 if __name__ == '__main__':
     # backtesting()
-    ta_bbands('ADBE', plot=True)
+    ta_bbands('PDD', plot=True)
