7c78a22c43fb77387462fd50c390921cacb114f2
Henry Lei <henrylei2000@gmail.com>
2025-02-03 19:46:56 -0800

    volume at reversals

--- research/strategies/raft_strategy.py
+++ research/strategies/raft_strategy.py
@@ -11,6 +11,7 @@ class RaftStrategy(Strategy):
         count = 0
         buckets_in_use = 0
         distance = 3
+        prominence = self.data.iloc[0]['close'] * 0.00125 + 0.005
         used_valley, used_peak = 0, 0
         checked_valley, checked_peak = 0, 0
 
@@ -21,28 +22,21 @@ class RaftStrategy(Strategy):
             prices = visible_rows['close']
             volumes = visible_rows['volume']
 
-            peaks, _ = find_peaks(prices, distance=distance)
-            valleys, _ = find_peaks(-prices, distance=distance)
+            peaks, _ = find_peaks(prices, distance=distance, prominence=prominence)
+            valleys, _ = find_peaks(-prices, distance=distance, prominence=prominence)
             volume_peaks, _ = find_peaks(volumes, distance=distance)
             volume_valleys, _ = find_peaks(-volumes, distance=distance)
 
-            if len(peaks) > 1 and len(valleys) > 1:
+            if len(peaks) > 1 and len(valleys) > 1 and len(volume_peaks) and len(volume_valleys):
 
                 valley, prior_valley, peak, prior_peak = valleys[-1], valleys[-2], peaks[-1], peaks[-2]
-
-                if len(visible_rows[visible_rows['volume_spike']]):
-                    spike_mean = int(visible_rows[visible_rows['volume_spike']]['volume'].mean()) // 1000
-                else:
-                    spike_mean = int(visible_rows['volume'].mean()) // 1000
-                if len(visible_rows[visible_rows['volume_drop']]):
-                    drop_mean = int(visible_rows[visible_rows['volume_drop']]['volume'].mean()) // 1000
-                else:
-                    drop_mean = int(visible_rows['volume'].mean()) // 1000
+                spike_mean = volumes.iloc[volume_peaks].mean() // 1000
+                drop_mean = volumes.iloc[volume_valleys].mean() // 1000
                 valley_volume = volumes.iloc[valley] // 1000
                 peak_volume = volumes.iloc[peak] // 1000
 
                 if valley > peak and buckets_in_use < self.num_buckets and valley != used_valley:  # from a valley
-                    if (valley_volume > spike_mean or valley_volume < drop_mean) and valley > checked_valley:
+                    if valley_volume > spike_mean and valley > checked_valley:
                         print(f"{count} spike {spike_mean}, drop {drop_mean}, {valley_volume} @ {valley}")
                         checked_valley = valley
 
@@ -55,7 +49,7 @@ class RaftStrategy(Strategy):
                         print(f"---- buy signal @ {count} from valley {valley}")
 
                 if valley < peak != used_peak and buckets_in_use:
-                    if (peak_volume > spike_mean or peak_volume < drop_mean) and peak > checked_peak:
+                    if peak_volume < drop_mean and peak > checked_peak:
                         checked_peak = peak
                         print(f"{count} spike {spike_mean}, drop {drop_mean}, {peak_volume} @ {peak}")
                         print(f"---- sell signal @ {count}")
@@ -77,7 +71,7 @@ class RaftStrategy(Strategy):
             count += 1
 
         data['position'] = positions
-        self.snapshot([260, 330], ['volume', 'gap'])
+        self.snapshot([150, 200], ['volume', 'gap'])
 
     def signal(self):
         self.raft()
